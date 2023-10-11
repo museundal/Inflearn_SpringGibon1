d2c82ebd79b1b23f92b359e8488414d6669e7d41
museundal <ehdus1105@gmail.com>
2023-10-11 23:22:28 +0900

    :hammer:객체지향의 원리 적용 2

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,26 +2,34 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     //1.어디선가 AppConfig를 통해서 멤버서비스를 불러다 쓸텐데 서비스Impl 구현체가 생성이 되면서 메모리 멤버 리포지토리도 들어간다.
     //2.그러면 멤버 서비스impl에  메모리멤버 리포지토리 코드는 없지만 자동 주입된다.
+    @Bean
     public MemberService memberService(){ //역할이 눈에 보임
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
-    private MemberRepository memberRepository(){ // 역할
+    @Bean
+    public MemberRepository memberRepository(){ // 역할
         return new MemoryMemberRepository();
     }
+    @Bean
     public OrderService orderService(){ // 역할
         return new OrderServiceImpl(new MemoryMemberRepository(),discountPolicy() );
-    }
+    }@Bean
     public DiscountPolicy discountPolicy(){//역할
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
