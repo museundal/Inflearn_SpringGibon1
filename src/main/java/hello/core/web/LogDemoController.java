d15e3d08acc0cc928b0d173eb3caecf69cd9907a
museundal <ehdus1105@gmail.com>
2023-11-04 22:53:19 +0900

    [:sparkles:] 스프링 기본편 완강

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -1,8 +1,8 @@
 package hello.core.web;
-
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,10 +13,13 @@ public class LogDemoController {
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
     @RequestMapping("log-demo")
-    @ResponseBody // 만들어진 화면이 없어서 만들어진 문자열 자체를 반환하려고 합니다.
+    @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
+
+        System.out.println("myLogger = " + myLogger.getClass());
+
         myLogger.log("controller test");
         logDemoService.logic("testId");
         return "OK";
